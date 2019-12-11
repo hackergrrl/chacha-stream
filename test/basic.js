aca4a60deb6cd2bbae1efe17ccb410fdea655523
noffle <sww@eight45.net>
2019-12-11 15:12:50 -0800

    test: add bad case

--- test/basic.js
+++ test/basic.js
@@ -17,3 +17,20 @@ test('can encrypt + decrypt', function (t) {
     t.same(data.toString(), 'hello world')
   })
 })
+
+test('can\'t decrypt with different key', function (t) {
+  t.plan(1)
+
+  let key0 = crypto.randomBytes(32)
+  let key1 = crypto.randomBytes(32)
+  let encode0 = Salsa.encoder(key0)
+  let decode0 = Salsa.decoder(key1)
+
+  encode0.write('hello world')
+
+  encode0.pipe(decode0)
+
+  decode0.once('data', function (data) {
+    t.notSame(data.toString(), 'hello world')
+  })
+})
